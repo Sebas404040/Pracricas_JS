fe2256403b7bfd6f0c38bcfb0df047067d31df16
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-06-04 22:27:37 -0500

    feat: :sparkles: commit del map y uso del for of
    commit
    
    none

--- practicarquiz.js
+++ practicarquiz.js
@@ -1,13 +1,37 @@
-const invitados = [
-  "Ana", "Luis", "Marta", "Luis", "Carlos", "Ana", "Sofía", "Carlos", "Luis"
-];
+const productos = new Map();
 
-const invitadosUnicos = new Set(invitados);
-const invitadosOrdenados = Array.from(invitadosUnicos).sort();
-console.log('Lista de invitados ordenada:');
-invitadosOrdenados.map((invitado, index) => {
-  console.log(`${index + 1}. ${invitado}`);
-});
+productos.set("camisa", "19.99");
+productos.set("pantalon", "29.99");
+productos.set("zapatos", "49.99");
+productos.set("chaqueta", "89.99");
+productos.set("bufanda", "15.99");
+productos.set("fideos", "12.99");
+
+productos.forEach((precio, producto) => {
+    console.log(`producto: ${producto}, precio: ${precio}`);
+})
+
+const verificar = productos.has("arroz");
+console.log(`¿Existe el producto "arroz"? ${verificar}`);
+
+productos.delete("fideos");
+
+productos.forEach((precio, producto) => {
+    console.log(`producto: ${producto}, precio: ${precio}`);
+})
+
+console.log(`Tamaño del Map: ${productos.size}`);
+
+for (const clave of productos.keys()) {
+    console.log(`Producto: ${clave}`);
+}
+
+for(const valor of productos.values()) {
+    console.log(`Precio: ${valor}`);
+}
+
+
+
 
 
 
